b67368376b3d127ea8dcef81aa7a5d304f6e00f3
loriyuhv <2385923602@qq.com>
2024-04-03 14:16:49 +0800

    字符串常量池何时引用

---
+++ z_Test/src/com/wsw/test/FTest.java
@@ -0,0 +1,16 @@
+package com.wsw.test;
+
+/**
+ * @author loriyuhv
+ * @ClassName FTest
+ * @date 2024/4/3 14:13
+ * @description 测试String
+ */
+
+public class FTest {
+    public static void main(String[] args) {
+        String str = "Jerry";
+        String str2 = new String("Jerry");
+        System.out.println(str == str2);
+    }
+}
